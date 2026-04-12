09669cb3bd20447d41d72e08971544d71ca707e2
manob21 <manobendra-2021911211@cs.du.ac.bd>
2026-04-12 18:38:28 +0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useEffect } from "react";
+import { supabase } from "./supabaseClient";
+
+function App() {
+  useEffect(() => {
+    async function testConnection() {
+      const { data, error } = await supabase
+        .from('test_table')   // you can change later
+        .select('*');
+
+      console.log("DATA:", data);
+      console.log("ERROR:", error);
+    }
+
+    testConnection();
+  }, []);
+
+  return <h1>Checking Supabase connection...</h1>;
+}
+
+export default App;
